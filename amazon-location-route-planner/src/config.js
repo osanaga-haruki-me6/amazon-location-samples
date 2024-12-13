b3610ca5b957c8143ed87132239038de6b0fbe15
osanaga.haruki <osanaga.haruki.me6@jp.nssol.nipponsteel.com>
2024-12-13 18:44:29 +0900

    mapの表示

--- amazon-location-route-planner/src/config.js
+++
@@ -1,5 +0,0 @@
-const identityPoolId = "IDENTITY_POOL_ID";
-const mapName = "TruckDemoMap";
-const routeCalculatorName = "TruckDemoRouteCalculator";
-
-export { identityPoolId, mapName, routeCalculatorName };
--- amplify-ui-geo-explore/src/configuration.js
+++ amplify-ui-geo-explore/src/configuration.js
@@ -1,21 +1,20 @@
 // This configuration file is a single place to provide any values to set up the app
 
-export const IDENTITY_POOL_ID =
-  "XX-XXXX-X:XXXXXXXX-XXXX-1234-abcd-1234567890ab"; // REQUIRED - Amazon Cognito Identity Pool ID
+export const IDENTITY_POOL_ID = "ap-northeast-1:9e900ac2-628e-4b29-ae62-97e0d4316d76"; // REQUIRED - Amazon Cognito Identity Pool ID
 
-export const REGION = "XX-XXXX-X"; // REQUIRED - Amazon Cognito Region
+export const REGION = "ap-northeast-1"; // REQUIRED - Amazon Cognito Region
 
 export const MAP = {
-  NAME: "XXXXXXXXXXX", // REQUIRED - Amazon Location Service map resource name
-  STYLE: "XXXXXXXXXXX", // REQUIRED - String representing the style of map resource
+  NAME: "20241213", // REQUIRED - Amazon Location Service map resource name
+  STYLE: "VectorEsriStreets", // REQUIRED - String representing the style of map resource
 };
 
-export const PLACE = "XXXXXXXXXXX"; // REQUIRED - Amazon Location Service place index resource name
+export const PLACE = "20241213"; // REQUIRED - Amazon Location Service place index resource name
 
-export const ROUTE = "XXXXXXXXXXX"; // REQUIRED - Amazon Location Service route calculator resource name
+export const ROUTE = "20241213"; // REQUIRED - Amazon Location Service route calculator resource name
 
-export const GEOFENCE = "XXXXXXXXXXX"; // REQUIRED - Amazon Location Service geofence collection resource name
+export const GEOFENCE = "20241213"; // REQUIRED - Amazon Location Service geofence collection resource name
 
-export const TRACKER = "XXXXXXXXXXX"; // REQUIRED - Amazon Location Service tracker resource name
+export const TRACKER = "20241213"; // REQUIRED - Amazon Location Service tracker resource name
 
-export const TRACKER_SIMULATED_DEVICE = "Vehicle-1"; // REQUIRED - Simulated Device ID (Defaulting to Vehicle-1)
+export const TRACKER_SIMULATED_DEVICE = "Vehicle-1";  // REQUIRED - Simulated Device ID (Defaulting to Vehicle-1)
